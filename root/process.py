ed2eb74b67a1376ed6fa6b60d7239b2987d8ba98
Julijana <julijana.jevtic@qcerris.com>
2024-05-28 16:25:59 +0200

    Getting zip through http get requests

---
+++ root/process.py
@@ -0,0 +1,33 @@
+import os
+import zipfile
+import tempfile
+import requests
+import logging
+from datetime import datetime
+
+log_file = 'processing.log'
+logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s - %(message)s')
+
+def add_separator_to_log():
+    separator = "\n" + "=" * 80 + "\n" + f"New Run: {datetime.now()}" + "\n" + "=" * 80 + "\n"
+    with open(log_file, 'a') as log:
+        log.write(separator)
+
+def download_zip(url, download_path):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(download_path, 'wb') as f:
+            f.write(response.content)
+        logging.info(f"Downloaded zip file from {url}")
+    else:
+        logging.error(f"Failed to download zip file from {url}, status code: {response.status_code}")
+        raise Exception(f"Failed to download zip file from {url}")
+
+add_separator_to_log()
+
+zip_file_url = "https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/1largeRjet1lep.zip"
+zip_file_path = os.path.join(os.getcwd(), "1largeRjet1lep.zip")
+output_dir = os.path.join(os.getcwd(), "output_1largeRjet1lep")
+os.makedirs(output_dir, exist_ok=True)
+download_zip(zip_file_url, zip_file_path)
+
